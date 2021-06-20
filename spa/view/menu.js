d02ccddd7da08a3d601262e5e42e4d91a207cd14
moovspace <moovspace@gmail.com>
2021-06-20 11:51:24 +0200

    App menu view

--- spa/view/menu.js
+++ spa/view/menu.js
@@ -2,16 +2,16 @@ import t from '/spa/router/translator.js'
 
 export default class MenuView
 {
-    static Html()
-    {
+	static Html()
+	{
 		return `<div id="links">
 			<a href="?lang=jp" id="lang" class="lang" data-lang="jp"> JP </a>
 			<a href="?lang=en" id="lang" class="lang" data-lang="en"> EN </a>
-            <a href="/">` + t.str("Home") + `</a>
-            <a href="/todos/list">` + t.str("TodosList") + `</a>
+			<a href="/">` + t.str("Home") + `</a>
+			<a href="/todos/list">` + t.str("TodosList") + `</a>
 			<a href="/profil/10">` + t.str("UserProfil") + `</a>
 			<a href="/upload">` + t.str("UploadFile") + `</a>
 			<a href="https://www.pagani.com">` + t.str("ExternalPage") + `</a>
-        </div>`;
-    }
+		</div>`;
+	}
 }
